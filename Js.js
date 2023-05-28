dda9aa771bc350147116063fadca80d7078166b1
mahmoudashraf211 <76871501+mahmoudashraf211@users.noreply.github.com>
2023-05-28 17:51:49 +0300

    Add files via upload

---
+++ Js.js
@@ -0,0 +1,9 @@
+
+function Calculate(){
+    let FirstNumber = parseFloat(document.getElementById("Number1").value);
+    let SecondNumber = parseFloat(document.getElementById("Number2").value);
+    let Result = FirstNumber + SecondNumber;
+    document.getElementById("Result").value = Result;
+}
+
+ 
